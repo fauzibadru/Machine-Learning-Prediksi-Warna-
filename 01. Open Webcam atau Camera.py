45f8fb286e1b6f7a845d8719df5aa3441804c0b4
Muhammad Fauzi Badru Zaman <mfauzibadruz@gmail.com>
2022-12-27 14:12:00 +0700

    Update 01. Open Webcam atau Camera.py

--- 01. Open Webcam atau Camera.py
+++ 01. Open Webcam atau Camera.py
@@ -1,11 +1,11 @@
 import cv2
 import numpy as np
-cap = cv2.VideoCapture (1)
+cap = cv2.VideoCapture (0)
 while True:
     ret, frame = cap.read()
     frame = cv2.flip (frame,1)
     cv2.imshow("camera", frame)
-    key = cv2.waitKey (1)
+    key = cv2.waitKey (0)
     if key == 27:
             break
 cap.release()
